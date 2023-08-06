9364321d5e514c06131df25b9695e89e06e99fd2
Richard L <richard.latham@yahoo.co.uk>
2023-08-06 21:05:06 +0100

    Remove a favourites.ts

---
+++ src/favourites.ts
@@ -0,0 +1,67 @@
+import { favourites as rawFavourites } from "./raw-favourites";
+
+function unpackVenue(venue: string) {
+    // To do consider proper implemenation of camel case 
+    return venue.replace("ROUNDABOUT", "Roundabout");
+}
+
+export type StartTimeT = string | null; // null -> More than one start time is listed
+
+function unpackStartTime(times: string): StartTimeT {
+    if (times.includes(" ")) {
+        return null;
+    }
+
+    return times;
+}
+
+export type DatesT = number[];
+function unpackDates(dates: string): DatesT {
+    const dateStr = dates.split(" ");
+    if (!dateStr[0]) {
+        return [];
+    }
+    // dateStr.map(parseInt) gived bad results because the index supplied by
+    // the map is treated as a radix.
+    return dateStr.map(d => parseInt(d));
+}
+
+/** Make a ShowInfo from raw info about one show */
+function makeShowInfo(line: string[]) {
+    if(line.length !== 7) {
+        throw new Error("Favourite array has unexpected size");
+    }
+    const obj = {
+        title: line[0],
+        venue: unpackVenue(line[1]),
+        duration: line[2],
+        startTime: unpackStartTime(line[3]),
+        dates: unpackDates(line[4]),
+        href: line[5],
+        rating: line[6],
+    }
+    return obj;
+}
+
+export type ShowInfo = ReturnType<typeof makeShowInfo>;
+
+export const favourites = rawFavourites.map(makeShowInfo);
+
+//https://stackoverflow.com/questions/34776846/how-to-freeze-nested-objects-in-javascript
+function deepFreeze (o: any) {
+  Object.freeze(o);
+  if (o === undefined) {
+    return o;
+  }
+
+  Object.getOwnPropertyNames(o).forEach(function (prop) {
+    if (o[prop] !== null
+    && (typeof o[prop] === "object" || typeof o[prop] === "function")
+    && !Object.isFrozen(o[prop])) {
+      deepFreeze(o[prop]);
+    }
+  });
+
+  return o;
+}
+deepFreeze(favourites);
