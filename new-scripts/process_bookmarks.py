b4f22f6033071967004d3aef562201f1b950fead
Richard L <richard.latham@yahoo.co.uk>
2025-07-26 16:14:36 +0100

    Bug fix and small changes

--- new-scripts/process_bookmarks.py
+++ new-scripts/process_bookmarks.py
@@ -3,7 +3,20 @@ import glob
 import sys
 import re
 
-from file_names import CHROME_BOOKMARKS_REGEX
+from file_names import CHROME_BOOKMARKS_REGEX, UNPROCESSED_BOOKMARKS
+
+def check_start_time(start_time):
+    """Check if the start time is in the correct format"""
+
+    if start_time.lower() == 'misc':
+        return 'misc'
+
+    # The expected format is 'HH:MM'
+    match = re.match(r'^\d{2}:\d{2}', start_time)
+    if match:
+        return start_time
+    
+    raise ValueError(f"Unexpected start time '{start_time}'")
 
 def get_name_and_url(line):
     """Extract name and URL from a bookmark line"""
@@ -20,7 +33,7 @@ def unpack_bookmark_name(bookmark_name):
     match = re.search(r'^([0-9]) +([^ ]+) +- +([^|]+)', bookmark_name)
     if match:
         rating = match.group(1)
-        start_time = match.group(2)
+        start_time = check_start_time(match.group(2))
         title = match.group(3).strip()
         if(rating and start_time and title):
             return {
@@ -38,7 +51,7 @@ def get_shows_from_bookmarks():
     filenames = glob.glob(CHROME_BOOKMARKS_REGEX)
 
     if len(filenames) == 0 :
-        print(f"Error: file called matching '{CHROME_BOOKMARKS_REGEX}' not found")
+        print(f"Error: No files matching '{CHROME_BOOKMARKS_REGEX}' found")
         sys.exit()
 
     if len(filenames) > 1 :
@@ -46,22 +59,30 @@ def get_shows_from_bookmarks():
         sys.exit()
 
     shows = {}
-    with open(filenames[0],encoding='UTF-8') as bookmarks:
-        try:
-            for line in bookmarks:
+    unprocessed_count = 0
+    with open(filenames[0],encoding='UTF-8') as bookmarks, \
+        open(UNPROCESSED_BOOKMARKS, 'w', encoding='UTF-8') as unprocessed:
+        for line in bookmarks:
+            try:
                 if 'https://www.edfringe.com' in line:
                     bookmark_name, url = get_name_and_url(line)
                     shows[url] = unpack_bookmark_name(bookmark_name)
-        except Exception as e:
-            print(f"Error processing bookmarks: {e} {line}")
+            except ValueError as e:
+                unprocessed.write(f"{line.strip()} {e}\n")
+                unprocessed_count += 1
+
+    if unprocessed_count > 0:
+        print(f"Warning: {unprocessed_count} bookmarks were not processed successfully.",
+              f"Check {UNPROCESSED_BOOKMARKS} for details.")
 
     return shows
 
 def main():
     """Main function to read and process bookmarks"""
     shows = get_shows_from_bookmarks()
-    for url, details in shows.items():
-        print(f"{url}: {details}")
+    print(f"{len(shows)} shows details extracted from bookmarks.")
+    # for url, details in shows.items():
+    #     print(f"{url}: {details}")
 
 if __name__ == "__main__":
     main()
